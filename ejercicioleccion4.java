0a2c52901b1183dd9ff28b47bde5872c682fad81
yohanres <110563311+yohanres@users.noreply.github.com>
2022-10-27 23:24:29 -0500

    Add files via upload

---
+++ ejercicioleccion4.java
@@ -0,0 +1,15 @@
+public class ejercicioleccion4 {
+
+    public static void main(String[] args) {
+        int numeroif = -4;
+
+        if  (numeroif > 0) {
+            System.out.println("es positivo");
+
+        } else if (numeroif < 0) {
+            System.out.println("es negativo");
+        } else {
+            System.out.println("es 0");
+        }
+    }
+}
